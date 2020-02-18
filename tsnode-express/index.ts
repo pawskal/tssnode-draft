5386b442d97e06ec7098f07b75f1dc5fdd35281a
pavel.ivanov <ipawskal@gmail.com>
2020-02-18 23:08:08 +0200

    chore: small fixes

--- tsnode-express/index.ts
+++ tsnode-express/index.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 import application from './server';
 import { ConfigProvider } from '../tsnode-core/lib';
-const {express, configProvider} = application.start()
+const {express, configProvider} = application.setup()
 
 http.createServer(express).listen(configProvider.port || 4000, () => {
   console.log(configProvider)
